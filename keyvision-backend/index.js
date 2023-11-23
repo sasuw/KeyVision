eb09873d39bc91532f19e7897df9d9eb3ab04bf2
Sasu Welling <sasu.welling@7p-group.com>
2023-11-23 21:01:54 +0100

    Added jsdoc

--- keyvision-backend/index.js
+++ keyvision-backend/index.js
@@ -1,27 +1,103 @@
-const express = require('express');
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUi = require("swagger-ui-express");
 const app = express();
 const port = 3000;
+
 const getApplications = require('./applications');
 const getShortcuts = require('./shortcuts');
 
+/**
+ * Route serving a health check.
+ * @name get/alive
+ * @function
+ * @memberof module:Express
+ * @inner
+ * @returns {string} - Confirmation that the server is running.
+ */
 app.get('/alive', (req, res) => {
     res.send('Server is up and running!');
 });
 
+/**
+ * Route serving the API version.
+ * @name get/version
+ * @function
+ * @memberof module:Express
+ * @inner
+ * @returns {string} - The current version of the API.
+ */
 app.get('/version', (req, res) => {
     res.send('API Version: 0.1.0');
 });
 
+/**
+ * Route serving a list of applications.
+ * @name get/applications
+ * @function
+ * @memberof module:Express
+ * @inner
+ * @returns {Object[]} - An array of application objects.
+ */
 app.get('/applications', (req, res) => {
     res.json(getApplications());
 });
 
+/**
+ * Route serving shortcuts for a specific application.
+ * @name get/applications/shortcuts/:appName
+ * @function
+ * @memberof module:Express
+ * @inner
+ * @param {string} appName - The name of the application.
+ * @returns {Object[]} - An array of shortcut objects for the given application.
+ */
 app.get('/applications/shortcuts/:appName', (req, res) => {
     const appName = req.params.appName;
     res.json(getShortcuts(appName));
 });
 
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "KeyVision Backend",
+            version: "0.1.0",
+            description:
+                "",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "7P FFM",
+                url: "https://7p-mobility.com",
+                email: "info@7p-mobility.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
 // Start the server
+/**
+ * Starts the Express server.
+ * @function
+ * @memberof module:Express
+ */
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
--- keyvision-backend/shortcuts.js
+++ keyvision-backend/shortcuts.js
@@ -1,18 +1,34 @@
 const KeyCode = require('keycode-js');
-const {all} = require("express/lib/application");
+const { all } = require("express/lib/application");
 
+/**
+ * Represents a keyboard shortcut.
+ * @class
+ * @param {string} name - The internal name of the shortcut.
+ * @param {string} displayName - The display name of the shortcut.
+ * @param {string} shortcutType - The type of the shortcut (e.g., 'default', 'user').
+ * @param {...number} keyCodes - Key codes that make up the shortcut.
+ */
 class Shortcut {
-    constructor(name, displayName, ...keyCodes) {
+    constructor(name, displayName, shortcutType, ...keyCodes) {
         this.name = name;
         this.displayName = displayName;
+        this.shortcutType = shortcutType;
         this.keyCodes = keyCodes;
     }
 }
 
+/**
+ * Retrieves shortcuts for a given application.
+ * @function getShortcuts
+ * @param {string} application - The name of the application.
+ * @returns {Shortcut[]} An array of Shortcut objects for the specified application.
+ */
 function getShortcuts(application) {
     if (application.toLowerCase() === 'exampleapp'.toLowerCase()) {
         return [
-            new Shortcut("newfile", "New File", [KeyCode.KEY_N, KeyCode.KEY_CONTROL])
+            new Shortcut("newfile", "New File", "default", [KeyCode.KEY_N, KeyCode.KEY_CONTROL]),
+            new Shortcut("save", "Save file", "user", [KeyCode.KEY_S, KeyCode.KEY_CONTROL])
         ];
     }
     return [];
