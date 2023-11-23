78176ea0fc1c8695d409690ee545e2e387e91598
Simon Heldt <simon.heldt@7p-group.com>
2023-11-24 00:49:21 +0100

    First draft frontend

---
+++ keyvision-frontend/src/components/Key.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+defineProps({
+  label: {
+    type: String,
+    required: false
+  },
+  keyCode: {
+    type: Number,
+    required: true
+  },
+  width: {
+    type: Number,
+    required: false,
+    default: 1
+  }
+})
+const keyWidth = 54;
+</script>
+
+<template>
+  <div v-if="keyCode != null" class="keyBorder" :style="{'width': keyWidth * width + 'px'}">
+    <div class="keyTop" :style="{'width': keyWidth * width + 'px'}">
+      <span class="keyLabel">{{label}}</span>
+    </div>
+  </div>
+  <div v-else class="keyBorder" :style="{'width': keyWidth * width + 'px'}">
+
+  </div>
+
+</template>
+
+<style scoped>
+  .keyBorder {
+    height: 54px;
+    background-color: #ccc;
+    border-width: 1px;
+    border-radius: 0px;
+    padding: 3px 6px;
+  }
+
+  .keyTop {
+    width: 42px;
+    height: 42px;
+    line-height: 1em;
+    background-color: #fcfcfc;
+    padding: 3px;
+  }
+
+  .keyLabel {
+    font-size: 12px;
+    color: #000;
+    white-space: pre-line;
+    margin-top: -5px;
+  }
+
+</style>
---
+++ keyvision-frontend/src/components/Keyboard.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Key from "@/components/Key.vue";
+
+const response = await fetch('/resources/keyboard.json');
+const keyboardData = await response.json();
+
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+<template>
+  <div class="keyboard">
+    <div v-for="row in keyboardData" class="row">
+      <Key v-for="key in row"
+           :key-code="key.keyCode"
+           :label="key.label"
+           :width="key.width" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.keyboard {
+  background-color: #ddd;
+  height: 361px;
+  width: 1255px;
+  border-radius: 6px;
+  padding: 9px;
+}
+.row {
+  display: flex;
+}
+
+</style>
