4e42d257ff8c5bf6ccd13bdf0ca49e9017a2dcd4
Sasu Welling <sasu.welling@7p-group.com>
2023-11-24 15:06:07 +0100

    Added keyboard event handler

--- keyvision-frontend/src/components/Keyboard.vue
+++ keyvision-frontend/src/components/Keyboard.vue
@@ -1,26 +1,69 @@
 <script setup>
+import { onMounted, onUnmounted } from 'vue';
 import Key from "@/components/Key.vue";
+import { ref } from 'vue';
 
-const response = await fetch('/resources/keyboard.json');
-const keyboardData = await response.json();
+const keyboardData = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('/resources/keyboard.json');
+    if (!response.ok) {
+      throw new Error('Failed to fetch');
+    }
+    const data = await response.json();
+    console.log("Fetched keyboard data:", data);
+    keyboardData.value = data;
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+//const response = await fetch('/resources/keyboard.json');
+//const keyboardData = await response.json();
 
 defineProps({
   name: {
     type: String,
     required: true
   }
-})
+});
+
+const handleKeyClick = (keyCode) => {
+  console.log('Key clicked:', keyCode);
+};
+
+const handleKeyPress = (event) => {
+  keyboardData.value.forEach(row => {
+    row.forEach(key => {
+      if (key.keyCode === event.keyCode) {
+        console.log('Key pressed:', key.keyCode);
+      }
+    });
+  });
+};
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyPress);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyPress);
+});
 </script>
+
 <template>
-  <div class="keyboard">
-    <div v-for="row in keyboardData" class="row">
-      <Key v-for="key in row"
-           :key-code="key.keyCode"
-           :label="key.label"
-           :width="key.width" />
+  <div class="keyboard" v-if="keyboardData.length > 0">
+    <div v-for="(row, rowIndex) in keyboardData" :key="rowIndex" class="row">
+      <Key v-for="key in row" :key="key.keyCode"
+           :key-code="key.keyCode" :label="key.label"
+           :width="key.width" @click="handleKeyClick(key.keyCode)" />
     </div>
   </div>
 </template>
+
+
 
 <style scoped>
 
